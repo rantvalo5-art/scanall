a606fa34fa44e84a02b2bcac8b563a362c286208
rantvalo5-art <rantvalo5@gmail.com>
2026-04-04 23:10:03 -0300

    config: intervals=[1m,15m,1h] via control panel

--- screener.py
+++ screener.py
@@ -16,7 +16,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 TELEGRAM_TOKEN   = os.environ["TELEGRAM_TOKEN"]
 TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-INTERVALS    = ["1h", "1m"]
+INTERVALS    = ["1m", "15m", "1h"]
 LIMIT        = 100
 TOP_N        = 9999
 MAX_WORKERS  = 20
